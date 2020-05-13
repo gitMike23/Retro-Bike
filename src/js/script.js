5028cb2170c1af424659ac4929e445aafabf5ed5
Mikhail Medovnik <medovnikfl@gmail.com>
2020-05-13 12:29:05 +0300

    First screen is done and adapted

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+// функция IBG для фоновой фулл-скрин картинки
+function ibg(){
+    $.each($('.ibg'), function(index, val) {
+        if($(this).find('img').length>0){
+            $(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
+        }
+    });
+}
+ibg();
+
+//смена классов
+$('.icon-menu').click(function(event){
+    $(this).toggleClass('active');
+    $('.menu__body').toggleClass('active');
+    $('body').toggleClass('lock');
+});
+
+
